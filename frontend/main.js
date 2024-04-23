7dee7c0ed6014668fb59113b3ede0074e99f46ea
Paakow Wobil <pwobil59@gmail.com>
2024-04-23 17:40:11 -0300

    Test

---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const localFunctionApi = 'http://localhost:7071/api/GetCounter';
+const functionApiUrl = "https://paakowazureresume.azurewebsites.net/api/GetCounter?code=kNrPQy8RJEv_PTWlKW-3a7KaPE90-nsjkixleXUSaJosAzFu56Ba6w=="
+
+const getVisitCount = () => {
+    let count = 30;
+
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then (response => {
+        console.log("Website called function API");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error)
+    });
+
+    return count;
+
+}
