e6b3d7905b5d1b9439a829f8433e93b72c0b4768
Paakow Wobil <pwobil59@gmail.com>
2024-04-25 16:46:00 -0300

    Deploying storage and functionality

--- backend/api/GetCounter.cs
+++ backend/api/GetCounter.cs
@@ -22,7 +22,7 @@ namespace Company.Function
             [CosmosDB(databaseName:"AzureResume", collectionName:"Counter", ConnectionStringSetting ="AzureResumeConnectionString", Id = "1", PartitionKey ="1")] out Counter updatedCounter,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a  request.");
 
             updatedCounter = counter;
             updatedCounter.Count += 1;
